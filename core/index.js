173e718c2776f6ad717fde6a88e9d8d0a89ff03a
taoweicn <twchn@live.com>
2018-04-24 00:15:51 +0800

    ⚡️ feat: able to check in more than one class automatically

--- core/index.js
+++ core/index.js
@@ -1,5 +1,6 @@
 const checkin = require('./checkin');
 const logger = require('../utils/logger');
+const delay = require('../utils/delay');
 
 main();
 
@@ -16,10 +17,12 @@ async function main() {
     switch (body[i]) {
       case 'repeat sign in':
         console.log('你已签过到了');
+        await delay(5 * 60 * 1000);  // 一般课堂签到限时5分钟
+        main();
         break;
       default:
         console.log(`第${body[i][4]}次签到成功，你是第${body[i][1]}位`);
-        logger(`${body[i][5]} 第${body[i][4]}次签到成功，名次第${body[i][1]}位`);
+        logger(`${body[i][5]} 第${body[i][4]}次签到成功，名次第${body[i][1]}位\n`);
     }
   }
 }
